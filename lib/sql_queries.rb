cb6dd54b69caca38975f1249e128c44290aa33d6
Clyde Griffin <deezy13@gmail.com>
2020-04-07 19:25:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ end
 def counts_number_of_bears_with_goofy_temperaments
  'SELECT COUNT(temperament)
   FROM bears 
-  WHERE temperment = "goofy"'
+  WHERE temperament = "goofy"'
 end 
 
 def selects_bear_that_killed_Tim
